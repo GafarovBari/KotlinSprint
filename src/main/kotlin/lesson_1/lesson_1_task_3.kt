afadbc6ef1ef0a44be0803e3294f1f4885c4a570
GafarovBari <2gafarovbari@gmail.com>
2025-02-12 16:37:26 +0300

    KS-1-3 Yuri Gagarin

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val yearOfFlight: Int = 1961
+    var hours: UInt = 9u
+    var minuts: UInt = 7u
+
+    println(yearOfFlight)
+    //println(hours)
+    //println(minuts)
+
+    hours = 10u
+    minuts = 55u
+
+    println("$hours:$minuts")
+
+}
