bae27dcb372aa8a149de98dd7231451f681434aa
GafarovBari <2gafarovbari@gmail.com>
2025-02-12 15:20:49 +0300

    KS-1-1 online store

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -2,4 +2,10 @@ package org.example.lesson_1
 
 fun main() {
 
+    var numberOfOrders: UInt = 75u
+    val thanksForThePurchase: String = "Спасибо за покупку! :)"
+
+    println(numberOfOrders)
+    println(thanksForThePurchase)
+
 }
