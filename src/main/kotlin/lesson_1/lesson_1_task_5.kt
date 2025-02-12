1698593d40a2b88c62a740356755db1cba724a66
GafarovBari <2gafarovbari@gmail.com>
2025-02-12 23:04:16 +0300

    KS-1-5 add constant and zero

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -3,9 +3,12 @@ package org.example.lesson_1
 fun main() {
 
     val seconds:Int = 6488
-    val hours:Int = (seconds/3600)
-    val minuts:Int = (6488-hours*3600)/60
-    val remainingSeconds:Int = 6488-hours*3600-minuts*60
+    val hours:Int = (seconds / THE_CONSTANT_TIME * THE_CONSTANT_TIME)
+    val minuts:Int = (seconds - hours * THE_CONSTANT_TIME * THE_CONSTANT_TIME) / THE_CONSTANT_TIME
+    val remainingSeconds:Int = seconds - hours * THE_CONSTANT_TIME * THE_CONSTANT_TIME - minuts * THE_CONSTANT_TIME
+    val addZero:Char = '0'
 
-    println("0$hours:$minuts:0$remainingSeconds")
+    println("$addZero$hours:$minuts:$addZero$remainingSeconds")
 }
+
+const val THE_CONSTANT_TIME:Byte = 60
