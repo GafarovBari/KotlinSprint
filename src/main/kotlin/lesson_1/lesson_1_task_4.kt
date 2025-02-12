3832a0c812cd2c04645c0ac4d7816169cf8fb85c
GafarovBari <2gafarovbari@gmail.com>
2025-02-12 18:02:50 +0300

    KS-1-3 flight report

---
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val trajectoryLenght: Long = 40868600000L
+    val crewMemberAge: Short = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double =  2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("""
+    $trajectoryLenght
+    $crewMemberAge
+    $partOfDay
+    $seconds
+    $partOfYear
+    $apogee
+    """.trimIndent())
+
+}
