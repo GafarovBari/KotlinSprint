ffcd1e6df03eeb8a853955463563845034b8aefe
GafarovBari <2gafarovbari@gmail.com>
2025-02-12 22:44:20 +0300

    KS-1-4 change format

--- src/main/kotlin/lesson_1/lesson_1_task_4.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -3,7 +3,7 @@ package org.example.lesson_1
 fun main() {
 
     val trajectoryLenght: Long = 40868600000L
-    val crewMemberAge: Short = 27
+    val crewMemberAge: Byte = 27
     val partOfDay: Float = 0.075f
     val seconds: Short = 6480
     val partOfYear: Double =  2.0547945205479453E-4
