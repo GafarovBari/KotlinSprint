952935a7cd77121e2c487c481ceef8e57f395992
GafarovBari <2gafarovbari@gmail.com>
2025-02-12 16:00:00 +0300

    KS-1-2 server logs

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+
+    var numberOfOrders: UInt = 75u
+    val thanksForThePurchase: String = "Спасибо за покупку! :)"
+    var currentStuff: UInt = 2000u
+
+    println(numberOfOrders)
+    println(thanksForThePurchase)
+//  println(currentStuff)
+
+    currentStuff = 1999u
+
+    println(currentStuff)
+
+}
