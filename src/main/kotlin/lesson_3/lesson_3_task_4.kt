d6b1b68c82ecc7cf6f1f18f66192e688fc01d940
GafarovBari <2gafarovbari@gmail.com>
2025-02-16 22:41:47 +0300

    KS-3-4 chess

---
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var beginningMove: String = "E2"
+    var endMove: String = "E4"
+    var numberMove: Char = '1'
+
+//  println("$beginningMove-$endMove:$numberMove")
+
+    beginningMove = "D2"
+    endMove = "D3"
+    numberMove = '2'
+
+    println("$beginningMove-$endMove:$numberMove")
+
+}
