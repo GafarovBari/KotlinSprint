937818b1f2d5a88744ce8e186f028e8021a976e7
GafarovBari <2gafarovbari@gmail.com>
2025-02-16 15:39:48 +0300

    KS-3-2 passport

---
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var name: String = "Андреева Татьяна Сергеевна"
+    var age: String = "20"
+
+//  println("$name,$age")
+
+    name = "Сидорова Татьяна Сергеевна"
+    age = "22"
+
+    println("$name,$age")
+
+}
