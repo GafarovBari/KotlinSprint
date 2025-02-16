52cb39fd9b056c5b405c6439dd99157de2c61261
GafarovBari <2gafarovbari@gmail.com>
2025-02-16 15:31:10 +0300

    KS-2-5 add const

--- src/main/kotlin/lesson_2/lesson_2_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_5.kt
@@ -7,14 +7,13 @@ fun main() {
     val depoositAmount: Double = 70000.0
     val interestRate: Double = 16.7
     val deposidPeriod: Double = 20.0
-    val totalAmount: Double
-    val income: Double
 
-    totalAmount = depoositAmount * (1 + interestRate / FULL_PERCENTAGE / CONSTANT_YEAR).pow(240)
-    income = totalAmount - depoositAmount
+    val totalAmount: Double = depoositAmount * (1 + interestRate / FULL_PERCENTAGE / CONSTANT_YEAR).pow(NUMBER_MONTHS)
+    val income: Double = totalAmount - depoositAmount
 
     println(String.format("%.3f", income))
 }
 
 const val FULL_PERCENTAGE = 100
 const val CONSTANT_YEAR = 12
+const val NUMBER_MONTHS = 240
