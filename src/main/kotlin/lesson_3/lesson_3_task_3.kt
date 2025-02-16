632365dd1ad6e3ae2cb11bb810e582c4b88fb0df
GafarovBari <2gafarovbari@gmail.com>
2025-02-16 15:46:38 +0300

    KS-3-3 table

---
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val number: Int = 3
+
+    println("""
+        |${number * 1}
+        |${number * 2}
+        |${number * 3}
+        |${number * 4}
+        |${number * 5}
+        |${number * 6}
+        |${number * 7}
+        |${number * 8}
+        |${number * 9}
+        |${number * 10}
+    """.trimMargin())
+
+}
+
