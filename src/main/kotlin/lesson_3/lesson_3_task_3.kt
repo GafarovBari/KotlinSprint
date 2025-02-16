ea59090f39aa65e077844c42953b508ca8eb6bf2
GafarovBari <2gafarovbari@gmail.com>
2025-02-16 22:32:25 +0300

    KS-3-3 new format

--- src/main/kotlin/lesson_3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -2,19 +2,19 @@ package org.example.lesson_3
 
 fun main() {
 
-    val number: Int = 3
+    val number: Int = 5
 
     println("""
-        |${number * 1}
-        |${number * 2}
-        |${number * 3}
-        |${number * 4}
-        |${number * 5}
-        |${number * 6}
-        |${number * 7}
-        |${number * 8}
-        |${number * 9}
-        |${number * 10}
+        |$number x 1 = ${number * 1}
+        |$number x 2 = ${number * 2}
+        |$number x 3 = ${number * 3}
+        |$number x 4 = ${number * 4}
+        |$number x 5 = ${number * 5}
+        |$number x 6 = ${number * 6}
+        |$number x 7 = ${number * 7}
+        |$number x 8 = ${number * 8}
+        |$number x 9 = ${number * 9}
+        |$number x 10 = ${number * 10}
     """.trimMargin())
 
 }
