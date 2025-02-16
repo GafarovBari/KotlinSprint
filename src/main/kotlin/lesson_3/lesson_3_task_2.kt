f19cc7acfcec646bcf3a6eccfc596ad9e0a735bc
GafarovBari <2gafarovbari@gmail.com>
2025-02-16 22:25:19 +0300

    KS-3-2 add names

--- src/main/kotlin/lesson_3/lesson_3_task_2.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -2,14 +2,15 @@ package org.example.lesson_3
 
 fun main() {
 
-    var name: String = "Андреева Татьяна Сергеевна"
-    var age: String = "20"
+    val surName: String = "Андреева"
+    val name: String = "Татьяна"
+    val middleName: String = "Сергеевна"
+    val age : String = "20"
+    val surName2: String = "Сидорова"
+    val name2: String = "Татьяна"
+    val middleName2: String = "Сергеевна"
+    val age2 : String = "22"
 
-//  println("$name,$age")
-
-    name = "Сидорова Татьяна Сергеевна"
-    age = "22"
-
-    println("$name,$age")
-
+    println("$surName $name $middleName,$age")
+    println("$surName2 $name2 $middleName2,$age2")
 }
