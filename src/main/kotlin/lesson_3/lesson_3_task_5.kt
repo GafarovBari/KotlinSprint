eecbcbb47f659c9180adccdaf7d2ba79cd7cebdf
GafarovBari <2gafarovbari@gmail.com>
2025-02-17 20:18:34 +0300

    KS-3-5 regex use

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -4,15 +4,9 @@ fun main() {
 
     val enemyMoveLogs: String = "D2-D4;0"
 
-    val motion = enemyMoveLogs.split(";")
-    val moveDirect = motion[0].split("-")
+    val (startMove, endMove, numberMove) = enemyMoveLogs.split(Regex("[-;]"))
 
-    val enemyMoveStart = moveDirect[0]
-    val enemyMoveEnd = moveDirect[1]
-    val enemyMotion = motion[1]
-
-    println(enemyMoveStart)
-    println(enemyMoveEnd)
-    println(enemyMotion)
-
+    println(startMove)
+    println(endMove)
+    println(numberMove)
 }
