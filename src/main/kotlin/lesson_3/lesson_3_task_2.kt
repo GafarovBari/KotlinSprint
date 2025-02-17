51ff08e219b8e0c91e1ab3063634d7ecfa9fe31b
GafarovBari <2gafarovbari@gmail.com>
2025-02-17 19:21:07 +0300

    KS-3-2 dell names

--- src/main/kotlin/lesson_3/lesson_3_task_2.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -2,15 +2,15 @@ package org.example.lesson_3
 
 fun main() {
 
-    val surName: String = "Андреева"
+    var surName: String = "Андреева"
     val name: String = "Татьяна"
     val middleName: String = "Сергеевна"
-    val age : String = "20"
-    val surName2: String = "Сидорова"
-    val name2: String = "Татьяна"
-    val middleName2: String = "Сергеевна"
-    val age2 : String = "22"
+    var age : String = "20"
+
+//  println("$surName $name $middleName,$age")
+
+    surName = "Сидорова"
+    age = "22"
 
     println("$surName $name $middleName,$age")
-    println("$surName2 $name2 $middleName2,$age2")
 }
