b0b71f058bbd7575f4774a2cdb6a7a91164f805f
GafarovBari <2gafarovbari@gmail.com>
2025-02-18 15:41:15 +0300

    KS-4-2 constant add

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -5,5 +5,9 @@ fun main() {
     val weight: Int = 50
     val volume: Int = 100
 
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Avarage': ${(35 < weight) and (weight <= 100) and (volume < 100)}")
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Avarage': ${(WEIGHT_WITHOUT_MIN < weight) and (weight <= WEIGHT_MAX) and (volume < VOLUME_WITHOUT_MIN)}")
 }
+
+const val WEIGHT_WITHOUT_MIN: Int = 35
+const val WEIGHT_MAX: Int = 100
+const val VOLUME_WITHOUT_MIN: Int = 100
