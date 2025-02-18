9a50547b090acd40d263fd101d607da8119ea023
GafarovBari <2gafarovbari@gmail.com>
2025-02-18 18:48:45 +0300

    KS-3-5 new split

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -3,8 +3,11 @@ package org.example.lesson_3
 fun main() {
 
     val enemyMoveLogs: String = "D2-D4;0"
-
-    val (startMove, endMove, numberMove) = enemyMoveLogs.split(Regex("[-;]"))
+    val moveParts = enemyMoveLogs.split("-", ";")
+    moveParts.toList<String>()
+    val startMove = moveParts[0]
+    val endMove = moveParts[1]
+    val numberMove = moveParts[2]
 
     println(startMove)
     println(endMove)
