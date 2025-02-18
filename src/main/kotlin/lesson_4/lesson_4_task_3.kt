3e4fd99deb3223eabc7da4289861cf4d39f47729
GafarovBari <2gafarovbari@gmail.com>
2025-02-18 15:52:20 +0300

    KS-4-3 new constant

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -7,7 +7,12 @@ fun main() {
     val airHumidity: Int = 20
     val season: String = "зима"
 
-    val conditions = (weather == true) and (awning == true) and (airHumidity == 20) and (season != "зима")
+    val conditions = (weather == NORMAL_WEATHER) and (awning == AWNING_IS_OPEN) and (airHumidity == AIR_HUMIDITY_NORMAL) and (season != OFF_SAESON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $conditions ")
 }
+
+const val NORMAL_WEATHER: Boolean = true
+const val AWNING_IS_OPEN: Boolean = true
+const val AIR_HUMIDITY_NORMAL: Int = 20
+const val OFF_SAESON: String = "зима"
