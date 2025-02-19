6156bdb94220316575fd1793e2212bf3ffbda212
GafarovBari <2gafarovbari@gmail.com>
2025-02-19 21:32:00 +0300

    KS-5-1 add random

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -1,10 +1,12 @@
 package org.example.lesson_5
 
+import kotlin.random.Random
+
 fun main() {
 
-    val taskOne: Int = 6
-    val taskTwo: Int = 7
-    val answer: Int = 13
+    val taskOne: Int = Random.nextInt(0,101)
+    val taskTwo: Int = Random.nextInt(0,101)
+    val answer: Int = taskOne + taskTwo
 
     println("Докажите, что вы не робот. Сложите два числа: $taskOne + $taskTwo ")
 
